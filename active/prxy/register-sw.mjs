501e71d04c5406c5ce712c6fdfbb88315a80c099
blockfreeubg <wrighth528+obamasoyummy@gmail.com>
2026-05-17 18:50:47 -0400

    Update stockSW URL to use CDN link

--- active/prxy/register-sw.mjs
+++ active/prxy/register-sw.mjs
@@ -2,7 +2,7 @@
 /**
  * Distributed with Ultraviolet and compatible with most configurations.
  */
-const stockSW = "/active/uv/sw.js";
+const stockSW = "https://cdn.jsdelivr.net/gh/blockfreeubg/flux-unblocker-static@main/active/uv/sw.js";
 
 /**
  * List of hostnames that are allowed to run serviceworkers on http://
